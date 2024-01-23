605f510d2fd360adb015c0ff56ab084e5f3893af
riicardods <riicardoljn@icloud.com>
2024-01-23 01:29:10 -0300

    Atualização

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 
 
-
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
